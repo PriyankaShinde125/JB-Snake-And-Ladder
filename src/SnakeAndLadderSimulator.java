e3f4c04df922b942d79da9660ccb20bdaf0045ef
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-18 18:24:06 +0530

    ADDED : Code to generate random die number

--- src/SnakeAndLadderSimulator.java
+++ src/SnakeAndLadderSimulator.java
@@ -1,6 +1,11 @@
 public class SnakeAndLadderSimulator {
+
     public static final int PLAYER_START_POSITION = 0;
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to Snake & Ladder Simulator");
+        int randomDie = (int) Math.floor(Math.random() * 6 + 1);
+        System.out.println(randomDie);
     }
 }
