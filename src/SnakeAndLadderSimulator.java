cb4727af8b5b3ec88091b05730fc7b854cf44780
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-18 18:16:19 +0530

    ADDED : Code to start single player play with start position 0

--- src/SnakeAndLadderSimulator.java
+++ src/SnakeAndLadderSimulator.java
@@ -1,4 +1,5 @@
 public class SnakeAndLadderSimulator {
+    public static final int PLAYER_START_POSITION = 0;
     public static void main(String[] args) {
         System.out.println("Welcome to Snake & Ladder Simulator");
     }
