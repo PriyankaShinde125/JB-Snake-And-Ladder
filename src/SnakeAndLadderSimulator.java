cf0556417edce73cbed9e4bcee8c83ec38060ce8
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-18 23:40:27 +0530

    ADDED : Code to store dice count with respective its position

--- src/SnakeAndLadderSimulator.java
+++ src/SnakeAndLadderSimulator.java
@@ -1,22 +1,35 @@
+import java.util.ArrayList;
+
 public class SnakeAndLadderSimulator {
     public static final int PLAYER_START_POSITION = 0;
     public static final int LADDER = 1;
     public static final int SNAKE = 2;
     public static final int WINNING_POSITION = 100;
     int currentPosition = PLAYER_START_POSITION;
+    int diceCount = 0;
+    ArrayList<Integer> positionAtDice;
 
     public static void main(String[] args) {
-        SnakeAndLadderSimulator simulatorObj = new SnakeAndLadderSimulator();
+
         System.out.println("Welcome to Snake & Ladder Simulator");
+
+        SnakeAndLadderSimulator simulatorObj = new SnakeAndLadderSimulator();
+        simulatorObj.positionAtDice = new ArrayList<>();
 
         while (simulatorObj.currentPosition < WINNING_POSITION) {
             simulatorObj.play();
         }
+
+        for (int dice = 0; dice < simulatorObj.positionAtDice.size(); dice++) {
+            System.out.println("Dice = " + dice + " -> " + simulatorObj.positionAtDice.get(dice));
+        }
     }
 
     public void play() {
+
         int randomDie = (int) Math.floor(Math.random() * 6 + 1);
         int move = (int) Math.floor(Math.random() * 3);
+
         switch (move) {
             case LADDER:
                 currentPosition = currentPosition + randomDie;
@@ -33,6 +46,6 @@ public class SnakeAndLadderSimulator {
                 break;
         }
 
-        System.out.println("Current position = " + currentPosition);
+        positionAtDice.add(currentPosition);
     }
 }
