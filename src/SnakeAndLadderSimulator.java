3d09581b477237b339c94d30da5a869c6500836b
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-18 22:25:20 +0530

    ADDED : Code to play until win and current position can not be negative

--- src/SnakeAndLadderSimulator.java
+++ src/SnakeAndLadderSimulator.java
@@ -2,25 +2,32 @@ public class SnakeAndLadderSimulator {
     public static final int PLAYER_START_POSITION = 0;
     public static final int LADDER = 1;
     public static final int SNAKE = 2;
+    public static final int WINNING_POSITION = 100;
 
     public static void main(String[] args) {
-        int currentPosition = PLAYER_START_POSITION;
+
+        int currentPosition=PLAYER_START_POSITION;
+
         System.out.println("Welcome to Snake & Ladder Simulator");
-        int randomDie = (int) Math.floor(Math.random() * 6 + 1);
-        int move = (int) Math.floor(Math.random() * 3);
-        switch (move) {
-            case LADDER:
-                currentPosition = currentPosition + randomDie;
-                break;
-            case SNAKE:
-                if (currentPosition - randomDie < 0)
-                    currentPosition = PLAYER_START_POSITION;
-                else
-                    currentPosition = currentPosition - randomDie;
-                break;
-            default:
-                break;
+
+        while (currentPosition <= WINNING_POSITION) {
+            int randomDie = (int) Math.floor(Math.random() * 6 + 1);
+            int move = (int) Math.floor(Math.random() * 3);
+            switch (move) {
+                case LADDER:
+                    currentPosition = currentPosition + randomDie;
+                    break;
+                case SNAKE:
+                    if (currentPosition - randomDie < 0)
+                        currentPosition = PLAYER_START_POSITION;
+                    else
+                        currentPosition = currentPosition - randomDie;
+                    break;
+                default:
+                    break;
+            }
+
+            System.out.println("Current position = " + currentPosition);
         }
-        System.out.println("Current position = "+currentPosition);
     }
 }
