f60db6306cd1167ac70281909d6db4549cfea422
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-18 19:00:34 +0530

    ADDED : Code to decide move and change current position

--- src/SnakeAndLadderSimulator.java
+++ src/SnakeAndLadderSimulator.java
@@ -1,11 +1,26 @@
 public class SnakeAndLadderSimulator {
-
     public static final int PLAYER_START_POSITION = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
 
     public static void main(String[] args) {
-
+        int currentPosition = PLAYER_START_POSITION;
         System.out.println("Welcome to Snake & Ladder Simulator");
         int randomDie = (int) Math.floor(Math.random() * 6 + 1);
-        System.out.println(randomDie);
+        int move = (int) Math.floor(Math.random() * 3);
+        switch (move) {
+            case LADDER:
+                currentPosition = currentPosition + randomDie;
+                break;
+            case SNAKE:
+                if (currentPosition - randomDie < 0)
+                    currentPosition = PLAYER_START_POSITION;
+                else
+                    currentPosition = currentPosition - randomDie;
+                break;
+            default:
+                break;
+        }
+        System.out.println("Current position = "+currentPosition);
     }
 }
